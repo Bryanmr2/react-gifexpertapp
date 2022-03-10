9aac23f1a31bee404e6c49ce2001048644edec88
Bryan Mendoza <mendozamr2@live.com>
2022-03-09 22:40:51 -0700

    docs agregado

---
+++ docs/precache-manifest.f74345171479f2aac72986a4497684e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dd33bb206ac7fc833202a2f62d35482",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b7f57b05e1aedcbb00c",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b7f57b05e1aedcbb00c",
+    "url": "/static/js/main.1db9def1.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
